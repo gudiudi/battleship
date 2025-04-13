0c7a319bf02bddb93e8a3614b5ec8f3780edfc0a
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-04-13 20:45:22 +0700

    feat: make #gameBoard on Participant class private

--- src/Participant.js
+++ src/Participant.js
@@ -1,21 +1,23 @@
 export default class Participant {
+	#gameBoard;
+
 	constructor(gameBoard) {
-		this.gameBoard = gameBoard;
+		this.#gameBoard = gameBoard;
 	}
 
 	placeShip(ship, x, y, dx = 0, dy = 1) {
-		return this.gameBoard.placeShip(ship, x, y, dx, dy);
+		return this.#gameBoard.placeShip(ship, x, y, dx, dy);
 	}
 
 	receiveAttack(x, y) {
-		return this.gameBoard.receiveAttack(x, y);
+		return this.#gameBoard.receiveAttack(x, y);
 	}
 
-	get board() {
-		return this.gameBoard.boardSnapshot;
+	get boardSnapshot() {
+		return this.#gameBoard.boardSnapshot;
 	}
 
 	get hasLost() {
-		return this.gameBoard.areAllShipsSunk;
+		return this.#gameBoard.areAllShipsSunk;
 	}
 }
