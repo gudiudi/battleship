ece646088230c534e0b9147615d49e8be42d419f
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-04-13 22:23:33 +0700

    feat: add Computer class

---
+++ src/Computer.js
@@ -0,0 +1,43 @@
+import Participant from "./Participant";
+
+export default class Computer extends Participant {
+	placeShip(ship) {
+		const cells = this.#getEmptyCells();
+		if (cells.length < 1) return false;
+
+		let placed = false;
+		while (!placed) {
+			const [x, y] = cells[Math.floor(Math.random() * cells.length)];
+			const isHorizontal = Math.random() < 0.5;
+			const dx = isHorizontal ? 0 : 1;
+			const dy = isHorizontal ? 1 : 0;
+
+			placed = super.placeShip(ship, x, y, dx, dy);
+		}
+
+		return placed;
+	}
+
+	makeAttack(opponent) {
+		let attacked = false;
+		while (!attacked) {
+			const x = Math.floor(Math.random() * this.boardSnapshot.length);
+			const y = Math.floor(Math.random() * this.boardSnapshot.length);
+			attacked = opponent.receiveAttack(x, y);
+		}
+
+		return attacked;
+	}
+
+	#getEmptyCells() {
+		const cells = [];
+		for (const [x, row] of this.boardSnapshot.entries()) {
+			for (const [y, cell] of row.entries()) {
+				if (cell) continue;
+				cells.push([x, y]);
+			}
+		}
+
+		return cells;
+	}
+}
