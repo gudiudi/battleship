c50fb0ac6708c0ff2ba495d9b3d6b3dc0b069db1
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-04-14 19:10:34 +0700

    feat: add mock main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,45 @@
+import Computer from "./Computer";
 import GameBoard from "./GameBoard";
 import Player from "./Player";
 import Ship from "./Ship";
 
-const gameBoard = new GameBoard();
-const player = new Player(gameBoard);
+const makeFleet = (participant) => {
+	const fleet = {
+		5: 2,
+		4: 3,
+		2: 4,
+		1: 5,
+	};
 
-const destroyer = new Ship(2);
-player.placeShip(destroyer, 0, 0);
+	for (const [ship, count] of Object.entries(fleet)) {
+		for (let i = 0; i < count; i++) {
+			participant.placeShip(new Ship(Number(ship)));
+		}
+	}
+};
 
-console.log(player.board);
+(() => {
+	const gameBoard = new GameBoard();
+	const player = new Player(gameBoard);
+
+	const carrier = new Ship(5);
+	const destroyer = new Ship(4);
+	const submarine = new Ship(3);
+	const patrolBoat = new Ship(2);
+
+	player.placeShip(carrier, 0, 0);
+	player.placeShip(destroyer, 1, 5);
+	player.placeShip(submarine, 2, 3);
+	player.placeShip(patrolBoat, 4, 5);
+
+	console.log(player.boardSnapshot);
+})();
+
+(() => {
+	const computerBoard = new GameBoard();
+	const computer = new Computer(computerBoard);
+
+	makeFleet(computer);
+
+	console.log(computer.boardSnapshot);
+})();
